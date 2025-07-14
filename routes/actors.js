4efef3bac5c42153fdb41470eb3e02f4ae525697
CodeWeissman <93421750+CodeWeissman@users.noreply.github.com>
2025-07-14 16:39:59 -0700

    Delete routes/actors.js
    wrong file

--- routes/actors.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { addActor } from '../controllers/actorsControllers.js'
-
-const router = express.Router();
-
-router.post('/', addActor);
-
-export default router;
