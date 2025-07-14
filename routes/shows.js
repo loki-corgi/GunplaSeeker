222402714e06fe8b8f11a130c228ffe5cc0d842d
CodeWeissman <93421750+CodeWeissman@users.noreply.github.com>
2025-07-14 16:39:43 -0700

    Delete routes/shows.js
    wrong file

--- routes/shows.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { addShow, getShowsJSON } from '../controllers/showsControllers.js'
-
-const router = express.Router();
-
-router.post('/', addShow);
-router.get('/', getShowsJSON);
-
-export default router;
