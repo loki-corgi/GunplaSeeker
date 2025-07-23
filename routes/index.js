ddc52d1423f878884ab6c3bac57a66e96ea97b4e
CodeWeissman <93421750+CodeWeissman@users.noreply.github.com>
2025-07-23 00:56:04 -0700

    Update index.js
    changed app.get to router.get

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ import { indexListing } from '../controllers/indexController.js';
 
 const router = express.Router();
 
-app.get('/', indexListing);
+router.get('/', indexListing);
 
 router.use('/api/v1', updateRoutes);
 router.use('/api/v1', retrieveRoutes)
