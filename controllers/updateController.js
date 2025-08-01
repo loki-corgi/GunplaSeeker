819332951f18f52392289c7bd88cc9b522814885
CodeWeissman <93421750+CodeWeissman@users.noreply.github.com>
2025-07-31 19:45:13 -0700

    Updated logic in code
    changed how storeName is handled so that if it's empty then we don't add it into the database

--- controllers/updateController.js
+++ controllers/updateController.js
@@ -1,7 +1,7 @@
 //needed in order to use Decimal128 for price
 import { Decimal128 } from 'mongodb';
 
-//routes for adding model into database
+//route for adding model listing into database
 const addModel = async (req, res) => {
     try {
         const database = req.app.locals.database;
@@ -12,22 +12,23 @@ const addModel = async (req, res) => {
             modelName: req.body.modelName,
             modelGrade: req.body.modelGrade,
             //what this does is that req.body.price is a string
-            //then it is passed into Number to change it to a number
+            //then it is passed into Number to change it to a number, if possible
             //then the toFixed(2) method from Number rounds the number to 2 decimal places and revert it back to string (toFixed() returns a string)
             //then Decimal128.fromString() grabs the resulting string from toFixed(2) and convert it to Decimal128
             price: Decimal128.fromString(Number(req.body.price).toFixed(2)),
             streetNumber: parseInt(req.body.streetNumber),
-            storeName: req.body.storeName,
             streetName: req.body.streetName,
             city: req.body.city,
             province: req.body.province
         };  
 
+        // Only add storeName if it exists and passes length check
+        if (req.body.storeName && req.body.storeName.length >= 2) {
+            model.storeName = req.body.storeName;
+        }
+
         await database.collection("gundam-models").insertOne(model);
         res.render('form'  , { message: `Successfully added ${model.modelName}` });
-
-        //when model is successfully added into database, redirect to index which will show the new listing with a message passed into query param
-        //res.redirect(`/?msg=Successfully+added+${model.name}`);
     }
     //this catch is used when inputting into database produced some schema validation error
     catch (e) {
@@ -51,11 +52,16 @@ const addModel = async (req, res) => {
                     message: prop.description
                 }));
 
-                res.status(500).render('error', { errors: errors });
+                res.status(400).render('error', { errors: errors });
+            }
+            else {
+                res.status(400).render('error', { errors: [{ field: 'error', 
+                    message: 'something other than propertiesNotSatisfied has been found'}] });
             }
         }
+        //this runs when the error is not from mongodb schema validation
         else {
-            res.status(500).render('error', { errors: [{field: 'error', message: 'something went wrong'}] });
+            res.status(400).render('error', { errors: [{field: 'error', message: 'something went wrong'}] });
         }
         
     }
